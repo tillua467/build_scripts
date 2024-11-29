a716af79409a6c81611b239f3ae3ec5aedddd476
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-11-29 17:07:40 +0600

    Create Phoenix-evoX.sh

---
+++ Phoenix-evoX.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/tillua467/local_manifests -b main .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# keys
+# mkdir vendor/lineage-priv
+# cp build-keys/* vendor/lineage-priv
+# echo "============="
+# echo "Keys copied"
+# echo "============="
+
+# Export
+export BUILD_USERNAME=tillua467 
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_phoenix-userdebug || lunch lineage_phoenix-ap2a-userdebug || lunch lineage_phoenix-ap3a-userdebug 
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+m evolution
