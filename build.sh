5bf16715bd89c0b96996387b268b7639cf0037b1
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-12-05 18:44:26 +0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "================================================"
+echo "  Files Are already Cloned Starting the build"
+echo "================================================"
+
+# Export
+echo "======= Exporting ======"
+export BUILD_USERNAME=tillua467 
+export BUILD_HOSTNAME=crave
+export TARGET_DISABLE_EPPE=true
+echo "======= Export Done ======"
+
+# Set up build environment
+echo "====== Starting  Envsetup  ======="
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+echo "=====Lunching========"
+lunch lineage_phoenix-ap3a-userdebug 
+echo "=====Lunching done========"
+
+
+# Build rom
+echo "===================================="
+echo "   STARTING Build EVOLUTION-X"
+echo "===================================="
+m evolution
