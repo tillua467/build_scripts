afa99efbb305460f0f0ec11456497edf03d501d6
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-12-05 23:12:11 +0600

    Update Phoenix-evoX.sh

--- Phoenix-evoX.sh
+++ Phoenix-evoX.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Upgrade System
+
+sudo apt update && sudo apt upgrade -y
+
+echo "========================================================================"
+echo "                        SYSTEM UPGRADED"
+echo "========================================================================="
+
+sudo apt install curl ccache
+git clone https://github.com/akhilnarang/scripts; cd scripts; sudo bash setup/android_build_env.sh
+cd ..
+
+echo "========================================================================"
+echo "                        NECESSARY STUFF'S "
+echo "========================================================================="
+
 
 # Remove Unnecessary Files
 echo "===================================="
@@ -9,6 +25,8 @@ rm -rf .repo/local_manifests
 rm -rf  vendor/xiaomi
 rm -rf  kernel/xiaomi
 rm -rf  device/xiaomi
+rm -rf device/xiaomi/sm6150-common
+rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
 
 echo "===================================="
@@ -20,9 +38,9 @@ echo "===================================="
 
 # repo init rom
 repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
-echo "=================="
-echo "Cloned Mniafest successfully"
-echo "=================="
+echo "=============================================="
+echo "       Cloned Mniafest successfully"
+echo "=============================================="
 
 # Local manifests
 git clone https://github.com/tillua467/local_manifests -b main .repo/local_manifests
@@ -31,18 +49,21 @@ echo "Local manifest cloned successfully"
 echo "============================"
 
 # Sync
-/opt/crave/resync.sh || repo sync
+/opt/crave/resync.sh && repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 echo "============="
 echo "Sync success"
 echo "============="
 
 # Export
+echo "======= Exporting........ ======"
 export BUILD_USERNAME=tillua467 
 export BUILD_HOSTNAME=crave
 export TARGET_DISABLE_EPPE=true
+export TZ=Asia/Dhaka
 echo "======= Export Done ======"
 
 # Set up build environment
+echo "====== Starting Envsetup ======="
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
@@ -51,11 +72,11 @@ lunch lineage_phoenix-ap3a-userdebug
 echo "=====Lunching done========"
 
 # Make cleaninstall
-# make installclean
-# echo "============="
+make installclean
+echo "============="
 
 # Build rom
 echo "===================================="
-echo "Building EVOLUTION-X..."
+echo "      Building EVOLUTION-X..."
 echo "===================================="
 m evolution
