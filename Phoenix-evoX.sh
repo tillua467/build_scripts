fac4a2a66b4aa1031f04c899e3dd9c34901a50f6
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-12-06 07:21:15 +0600

    Update Phoenix-evoX.sh

--- Phoenix-evoX.sh
+++ Phoenix-evoX.sh
@@ -18,7 +18,11 @@ rm -rf  device/xiaomi
 rm -rf device/xiaomi/sm6150-common
 rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
-rm -rf out/target/product/phoenix
+rm -rf out/target/product/*/*zip
+rm -rf out/target/product/*/*txt
+rm -rf out/target/product/*/boot.img
+rm -rf out/target/product/*/recovery.img
+rm -rf out/target/product/*/super*img
 
 echo "===================================="
 echo "  Removing Unnecessary Files Done"
